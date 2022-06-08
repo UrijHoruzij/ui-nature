8f7bc3958921975c81651b8f87001e545731f04b
Юрий Хоружий <urijhoruzij@gmail.com>
2022-06-08 16:56:19 +0300

    fix: remove node-sass

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,9 @@
 import { addDecorator } from "@storybook/react";
 import { withThemesProvider } from "storybook-addon-styled-component-theme";
 import { ThemeProvider } from "styled-components";
-import { Theme } from "../src/components";
+// import { Theme } from "../src/components";
 
-const themes = [Theme];
+const themes = [{}];
 addDecorator(withThemesProvider(themes), ThemeProvider);
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
